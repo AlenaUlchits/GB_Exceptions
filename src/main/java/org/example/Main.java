7d55820b6b2619fc2a2701d46d55fcecb6c39d4e
Alena Ulchits <alena.ulchits@innowise-group.com>
2023-10-06 13:12:52 +0300

    task 1,4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,17 +10,18 @@ public class Main {
 
     private static void emptyString(String number) throws Exception{
         if(number.isEmpty()){
-
+            throw new Exception("The string must not be empty");
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Scanner sc = new Scanner(System.in);
 
         Boolean exit = Boolean.FALSE;
         while (!exit) {
             System.out.println("Please enter a number: ");
             String number = sc.nextLine();
+            emptyString(number);
             try{
                 System.out.println("Your number is: " + parseToNumber(number));
                 exit = Boolean.TRUE;
